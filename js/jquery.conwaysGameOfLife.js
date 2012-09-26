aa6d0855992187910f0a499acec8dbf05a74ad4f
Brad Kahl <bkahl1984@gmail.com>
2012-09-26 04:03:21 -0400

    updated styles, still need to tweak the fadin and fadeout.

--- js/jquery.conwaysGameOfLife.js
+++ js/jquery.conwaysGameOfLife.js
@@ -1,5 +1,183 @@
+var z = 0, max = 15;
+
 function GameList(){
     this.head = null;
+}
+
+function getSurroundingCells(allCells, rows, columns, z){
+	
+	if(!allCells) { return; }
+	
+	var validSurroundingCells = [{}], 
+		count = 1;
+	
+	allCells.forEach(function(o){
+		
+		console.log('cells : ',o.row+', '+o.col);
+		
+			var cRow		=	o.row, 
+				cCol		=	o.col,
+				cells		=	new GameList(),
+				dataHash,
+				surroundingCells;
+			
+		surroundingCells = [
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+cRow+'c'+(cCol+1),			row: cRow,		col: cCol+1 },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow+1)+'c'+(cCol+1),		row: cRow+1,	col: cCol+1 },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow+1)+'c'+cCol,			row: cRow+1,	col: cCol },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow+1)+'c'+(cCol-1),		row: cRow+1,	col: cCol-1 },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+cRow+'c'+(cCol-1),			row: cRow,		col: cCol-1 },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow-1)+'c'+(cCol-1),		row: cRow-1,	col: cCol-1 },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow-1)+'c'+cCol,			row: cRow-1,	col: cCol },
+			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow-1)+'c'+(cCol+1),		row: cRow-1,	col: cCol+1 }
+		];
+	
+		surroundingCells.forEach(function(e){
+			
+			var cell;
+			
+			if(e.row > rows) { console.log('row is outside of container'); }
+			else if(e.col > columns) { console.log('column is outside of container'); }
+			else if(e.row <= 0) { console.log('row is less then or equal to 0'); }
+			else if(e.col <= 0) { console.log('column is less then or equal to 0'); }
+			else {		
+				cell = { id: e.id, cell: { row: e.row, col: e.col, state: $('#'+z+' tr.'+e.row+' td.'+e.col).attr('class').split(" ")[1] } };
+				cells.insert(cell);
+			}
+		});
+		
+		dataHash = cells.createDataHash();
+		validSurroundingCells[count] = { id: dataHash.id, row: dataHash.id.split('cr')[1].split('cc')[0], col: dataHash.id.split('cr')[1].split('cc')[1], surroundingCells: dataHash.surroundingCells, state: $('#'+z+' tr.'+cRow+' td.'+cCol).attr('class').split(" ")[1] };
+		console.log('validSurroundingCells['+count+'] : ',validSurroundingCells[count]);
+		count++;
+		
+	});
+	
+	return validSurroundingCells;
+	
+}
+
+function getAllGridPositionsOnBoard(rowSize,colSize){
+	if(rowSize === 0 || colSize === 0) { return; }
+	
+	var allCells = [],
+		cellAmount = (rowSize*colSize),
+		i, r, c;
+	
+	for(r=1; r<=rowSize; r++){
+		for(c=1; c<=colSize; c++){
+			allCells.push({ row: r, col: c });
+			console.log('row : '+r+', col : '+c);
+		}
+	}
+	return allCells;
+}
+
+function buildBoard(id, rows, columns, z){
+	if(rows === 0 || columns === 0) { return; }
+	
+	var table, r, c;
+		
+	table = "<table id='"+z+"' style='position: absolute; z-index:"+(max-=1)+"; visibility: hidden;'>";
+		table += "<tbody>";
+			for(r=1; r<=rows; r++){
+				table += "<tr class='"+r+"'>";
+					for(c=1; c<=columns; c++){
+						table += "<td class='"+c+" dead'><img src='images/dead-cell.png'/></td>";
+					}
+				table += "</tr>";
+			}
+		table += "</tbody>";
+	table += "</table>";
+	
+	$(id).append(table);
+	
+	if(z-1 !== 0){
+		$('#'+z).show((z*z)*1000, function(){
+			$(this).css('visibility','visible').fadeIn("fast");
+		});
+		$('#'+(z-1)).hide((z*z)*500, function(){
+			$(this).css('display','hidden').fadeOut("fast");
+		});
+		
+	}
+	// if(z > 1){
+	// 	
+	// 	$('#'+(z-1)).fadeOut((z-1)*3000);
+	// 	$('#'+z).fadeIn(z*1000);
+	// }else{ $(id).append(table); }
+
+}
+
+function createGame(id, opts, z){
+	
+	z += 1;
+	
+	console.log('create game : ',opts);
+	
+    var width					= opts.board.rowAndColSize.width, 
+        height					= opts.board.rowAndColSize.height,
+        rows					= opts.board.rowAndCol.rows,
+        columns					= opts.board.rowAndCol.cols,
+		allGridCells			= getAllGridPositionsOnBoard(rows,columns),
+        livingCells				= opts.board.livingCells,
+		cellImg					= "<img src='images/cell-icon.png'/>",
+		game					= new GameList(),
+		validSurroundingCells	= [],
+		surroundingCells, i,
+		board = {
+			"id": id,
+			"rowAndCol": {"rows": rows, "cols": columns },
+			"rowAndColSize": {"width": width, "height": height}
+		}, currentBorderColumn, currentBorderRow;
+
+	buildBoard(id, rows, columns, z);
+
+	// for(currentBorderRow=1;currentBorderRow<=rows;currentBorderRow++){
+	// 	if(currentBorderRow===1){
+	// 		$("#"+z+" tr."+currentBorderRow).css('height',height).css('border-top','1px solid #000').css('border-bottom','1px solid #000');
+	// 	}else{
+	// 		$("#"+z+" tr."+currentBorderRow).css('height',height).css('border-bottom','1px solid #000');
+	// 	}
+	// 	$("#"+z+" tr").css('box-sizing','border-box').css('height',height).css('border-top','1px solid #000').css('border-bottom','1px solid #000');
+	// 	for(currentBorderColumn=1;currentBorderColumn<=columns;currentBorderColumn++){
+	// 		if(currentBorderColumn===1){
+	// 			$("#"+z+" tr."+currentBorderRow+" td."+currentBorderColumn).css('width',width).css('box-sizing','border-box').css('border-left','1px solid #000').css('border-right','1px solid #000');	
+	// 		}else{
+	// 			$("#"+z+" tr."+currentBorderRow+" td."+currentBorderColumn).css('width',width).css('box-sizing','border-box').css('border-right','1px solid #000');	
+	// 		}
+	// 	}
+	// }
+	
+	$(id).css('margin','100px auto 0').css('width',(columns*parseInt(width.split("px")[0],10))+"px");
+	$("table tr").css('height',height).css('border-top','1px solid #000');
+	$("table tr td").css('width',width).css('border-right','1px solid #000');
+	$("table").css('border-bottom','1px solid #000').css('border-left','1px solid #000');
+	
+	livingCells.forEach(function(o){
+		var cRow	=	o.row, 
+			cCol	=	o.col;
+			
+		$('#'+z+' tr.'+cRow+ ' td.'+cCol).removeClass('dead')
+								.addClass('alive')
+								.empty()
+								.append(cellImg);
+	});
+	
+	surroundingCells = getSurroundingCells(allGridCells, rows, columns, z);
+
+	for(i=surroundingCells.length-1; i>=1; i--){
+		if(surroundingCells[i] === undefined) { return alert('undefined'); }
+		validSurroundingCells.push(surroundingCells[i]);
+	}
+	
+	console.log('valid surrounding cells : ',validSurroundingCells);
+	
+	game.insert(validSurroundingCells);
+	
+	if(z===15){ return; }
+	
+	game.nextGameDataHash(board, z);
 }
 
 GameList.prototype = {
@@ -34,7 +212,7 @@ GameList.prototype = {
      */
 
     showAllElements:function(){
-        if (this.head === null) return;
+        if (this.head === null) { return; }
         var current = this.head, i = 1;
         while(current.next !== null){
             console.log('The element at position ' + i + ' has value ' + current.value);
@@ -49,7 +227,7 @@ GameList.prototype = {
      * create data hash of all valid surrounding cells of a cell
      */
 	createDataHash:function(){
-        if (this.head === null) return;
+        if (this.head === null) { return; }
         var current = this.head, i = 1, surroundingCells = [{}];
 
 		surroundingCells.id = current.value.id.split("-")[0];
@@ -71,13 +249,13 @@ GameList.prototype = {
     /**
      * create data hash for next game and determine if cell lives or dies
      */	
-	nextGameDataHash:function(board){
-        if (this.head === null) return;
+	nextGameDataHash:function(board, z){
+        if (this.head === null) { return; }
         var current = this.head, 
 			i, 
 			lengthOfCells = current.value.length,
 			nextGameCells = [],
-			id = board.id,
+			boardId = board.id,
 			opts = {
 				"board": {
 					"rowAndCol": { "rows": board.rowAndCol.rows, "cols": board.rowAndCol.cols },
@@ -99,7 +277,6 @@ GameList.prototype = {
 				id = cell.id,
 				surroundingCellsLength = cell.surroundingCells.length,
 				sc,
-				nextGameCell = [{}],
 				alive = 0,
 				dead = 0,
 				nextState;
@@ -110,8 +287,8 @@ GameList.prototype = {
 				var surroundingCell = cell.surroundingCells[sc],
 					surroundingCellState = surroundingCell.state;
 					
-				if(surroundingCellState === "alive") alive +=1;
-				else if(surroundingCellState === "dead") dead +=1;
+				if(surroundingCellState === "alive") { alive +=1; }
+				else if(surroundingCellState === "dead") { dead +=1; }
 			}
 			
 			console.log('alive cells : ',alive);
@@ -140,165 +317,20 @@ GameList.prototype = {
 					nextState = "dead";
 				}
 			}
+			
+			if(nextState === "alive") { opts.board.livingCells[i] = { "row": row, "col": col }; }
 					
-			nextGameCell.col = col,
-			nextGameCell.row = row,
-			nextGameCell.state = nextState;
-			
-			if(nextState === "alive") opts.board.livingCells[i] = { "row": row, "col": col };
-			
-			nextGameCells.push(nextGameCell);
-			
-			console.log('nextState : ',nextState);
-			
 			nextState = null;
 			alive = 0;
 			dead = 0;
 		}
-		console.log('nextGameCells : ',nextGameCells);
+		
 		console.log('opts : ',opts);
 		
-		createGame(id, opts);
+		console.log('z : ',z);
+		createGame(boardId, opts, z);
 	}
 };
-
-function getSurroundingCells(allCells, rows, columns){
-	
-	if(!allCells) return;
-	
-	var validSurroundingCells = [{}], 
-		count = 1;
-	
-	allCells.forEach(function(o){
-		
-		console.log('cells : ',o.row+', '+o.col);
-		
-		var cRow 	= o.row, 
-			cCol 	= o.col,
-			cells	= new GameList(),
-			dataHash,
-			surroundingCells;
-			
-		surroundingCells = [
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+cRow+'c'+(cCol+1),			row: cRow,		col: cCol+1 },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow+1)+'c'+(cCol+1),		row: cRow+1, 	col: cCol+1 },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow+1)+'c'+cCol,			row: cRow+1, 	col: cCol },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow+1)+'c'+(cCol-1),		row: cRow+1, 	col: cCol-1 },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+cRow+'c'+(cCol-1),			row: cRow, 		col: cCol-1 },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow-1)+'c'+(cCol-1),		row: cRow-1, 	col: cCol-1 },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow-1)+'c'+cCol,			row: cRow-1, 	col: cCol },
-			{ id: 'cr'+cRow+'cc'+cCol+'-r'+(cRow-1)+'c'+(cCol+1),		row: cRow-1, 	col: cCol+1 }
-		];
-	
-		surroundingCells.forEach(function(e){
-			
-			var cell;
-			
-			if(e.row > rows) console.log('row is outside of container');
-			else if(e.col > columns) console.log('column is outside of container');
-			else if(e.row <= 0) console.log('row is less then or equal to 0');
-			else if(e.col <= 0) console.log('column is less then or equal to 0');
-			else {		
-				cell = { id: e.id, cell: { row: e.row, col: e.col, state: $('#'+e.row+' .'+e.col).attr('class').split(" ")[1] } };
-				cells.insert(cell);
-			}
-		});
-		
-		dataHash = cells.createDataHash();
-		validSurroundingCells[count] = { id: dataHash.id, row: dataHash.id.split('cr')[1].split('cc')[0], col: dataHash.id.split('cr')[1].split('cc')[1], surroundingCells: dataHash.surroundingCells, state: $('#'+cRow+' .'+cCol).attr('class').split(" ")[1] };
-		console.log('validSurroundingCells['+count+'] : ',validSurroundingCells[count]);
-		count++;
-		
-	});
-	
-	return validSurroundingCells;
-	
-}
-
-function getAllGridPositionsOnBoard(rowSize,colSize){
-	if(rowSize === 0 || colSize === 0) return;
-	
-	var allCells = [],
-		cellAmount = (rowSize*colSize),
-		i, r, c;
-	
-	for(r=1; r<=rowSize; r++){
-		for(c=1; c<=colSize; c++){
-			allCells.push({ row: r, col: c });
-			console.log('row : '+r+', col : '+c);
-		}
-	}
-	return allCells;
-}
-
-function buildBoard(id, rows, columns){
-	if(rows === 0 || columns === 0) return;
-	
-	var table, r, c;
-	
-	table = "<table>";
-		table += "<tbody>";
-			for(r=1; r<=rows; r++){
-				table += "<tr id='"+r+"'>";
-					for(c=1; c<=columns; c++){
-						table += "<td class='"+c+" dead'><img src='images/x-icon.png'/></td>";
-					}
-				table += "</tr>";
-			}
-		table += "</tbody>";
-	table += "</table>";
-	
-	$(id).append(table);
-}
-
-function createGame(id, opts){
-	
-	console.log('create game : ',opts);
-	
-    var width					= opts.board.rowAndColSize.width, 
-        height					= opts.board.rowAndColSize.height,
-        rows					= opts.board.rowAndCol.rows,
-        columns					= opts.board.rowAndCol.cols,
-		allGridCells			= getAllGridPositionsOnBoard(rows,columns),
-        livingCells				= opts.board.livingCells,
-		cellImg					= "<img src='images/cell-icon.png'/>",
-		game					= new GameList(),
-		validSurroundingCells 	= [],
-		surroundingCells, i,
-		board = {
-			"id": id,
-			"rowAndCol": {"rows": rows, "cols": columns },
-			"rowAndColSize": {"width": width, "height": height}
-		};
-		
-	
-	buildBoard(id, rows, columns);
-
-	$("table tr").css('height',height);
-	$("table tr td").css('max-width',width).css('padding','10px').css('border','1px solid #000');
-	
-	livingCells.forEach(function(o){
-		var cRow 	= o.row, 
-			cCol 	= o.col;
-			
-		$('#'+cRow+ " ."+cCol).removeClass('dead')
-								.addClass('alive')
-								.empty()
-								.append(cellImg);
-	});
-	
-	surroundingCells = getSurroundingCells(allGridCells, rows, columns);
-
-	for(i=surroundingCells.length-1; i>=1; i--){
-		if(surroundingCells[i] === undefined) return alert('undefined');
-		validSurroundingCells.push(surroundingCells[i]);
-	}
-	
-	console.log('valid surrounding cells : ',validSurroundingCells);
-	
-	game.insert(validSurroundingCells);
-	game.nextGameDataHash(board);
-}
 
 (function($){
     $.fn.conwaysGameOfLife = function(options){
@@ -306,17 +338,50 @@ function createGame(id, opts){
 
         $.fn.conwaysGameOfLife.defaults = {
             board: { 
-               rowAndCol: {rows: 2, cols: 2},
-               rowAndColSize: {width: '50px', height: 'auto'},
-               livingCells: [{row: 1, col: 1}, {row: 1, col: 2}, {row: 2, col: 1}]
+               rowAndCol: {rows: 10, cols: 10},
+               rowAndColSize: {width: '50px', height: '50px'},
+               livingCells: [	{row: 1, col: 1}, 
+								{row: 1, col: 2},
+								{row: 1, col: 10},
+								{row: 1, col: 5},
+								{row: 2, col: 1},
+								{row: 2, col: 8},
+								{row: 3, col: 3},
+								{row: 2, col: 6},
+								{row: 2, col: 7},
+								{row: 2, col: 9},
+								{row: 4, col: 8},
+								{row: 4, col: 4},
+								{row: 5, col: 5},
+								{row: 5, col: 4},
+								{row: 6, col: 9},
+								{row: 6, col: 8},
+								{row: 6, col: 10},
+								{row: 7, col: 8},
+								{row: 7, col: 5},
+								{row: 7, col: 1},
+								{row: 8, col: 3},
+								{row: 8, col: 10},
+								{row: 8, col: 7},
+								{row: 9, col: 5},
+								{row: 9, col: 1},
+								{row: 9, col: 10},
+								{row: 10, col: 1},
+								{row: 10, col: 3},
+								{row: 10, col: 7},
+								{row: 10, col: 8},
+								{row: 10, col: 10},
+								{row: 2, col: 1}]
             }            
         };
 
         opts = $.extend({}, $.fn.conwaysGameOfLife.defaults, options);
 
-        createGame(id, opts);
+        createGame(id, opts, z);
     
         return this;    
     };
 
-})(jQuery);
+}(jQuery));
+
+
